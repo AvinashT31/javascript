5aaad6091c7952bf836782d56333db7a081a7c78
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-02-18 13:57:22 +0530

    practise

---
+++ functions.js
@@ -0,0 +1,44 @@
+// string, Number
+// var a= 20;
+// var b= "harry";
+
+
+// to store the variable
+// var a= 20;
+// var b= 30;
+// console.log(a);
+// console.log(b);
+
+
+//  var a= 20;
+//  var b= 30;
+//  var c= a+b;
+//  console.log(c);
+
+
+// To measure the length of the string
+// var a = "this is the first paragraph";
+// var b = a.length;
+// console.log(b);
+
+
+// to measure the first occurance of the string
+// var a = "this is the first paragraph";
+// var b = a.indexOf("is");
+// console.log(b);
+
+
+// to measure the first occurance of the string
+// var a = "this is the first paragraph";
+// var c = a.lastIndexOf("is");
+// console.log(c);
+
+
+// constant variable(vslue does not changed)
+// const a=20;
+// console.log(a);
+
+
+// var a="this is the string";
+// var b= a.slice(1,6);
+// console.log(b);
