06253bb834a8efa5063c9193b085f26a52b3c105
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-02-22 23:09:25 +0530

    update

--- practise todo function/index.js
+++ practise todo function/index.js
@@ -14,12 +14,74 @@ function addTodo(){
         console.log(myTodosFromLS);
         localStorage.setItem("Todo", JSON.stringify(myTodosFromLS));
         document.getElementById("TodoHere").value = "";
+        cs();
         alert('Added');        
     }else{
         alert('Add Todo First');
     }
 }
 
+function cs(){
+      var divFromhtml = document.getElementById("displayTodos");
+
+      var gettingTodosFromLS = JSON.parse(localStorage.getItem("Todo"));
+       console.log(gettingTodosFromLS,"gettingTodosFromLS");
+
+      var finalstructure = [`<div id="row">
+          <p>task</p> <p>completed</p>
+          </div>`]
+
+      for(var i=0; i < gettingTodosFromLS.length; i++){
+       finalstructure += `<div id="content">
+       <p>${gettingTodosFromLS[i]}</p>
+       <i onclick = "deleteTodo(${i})" class="fa-solid fa-trash"></i> 
+       </div>`
+    }
+    divFromhtml.innerHTML = finalstructure;
+}
+cs()
+
+
+function deleteTodo(todoIndex){
+    var todosFromLS = JSON.parse(localStorage.getItem("Todos"));
+    todosFromLS.splice(todoIndex, 1)
+    localStorage.setItem("Todos", JSON.stringify(todosFromLS));
+    cs(); 
+}
+
+
+// function cs(){
+     
+//     var divFromhtml = document.getElementById("displayTodos")
+
+//      var gettingTodosFromLS = JSON.parse(localStorage.getItem("Todo"));
+//      console.log(gettingTodosFromLS, "gettingTodosFromLS");
+ 
+//      var finalstructure = [`<div id="row">
+//      <p>task</p> <p>completed</p>
+//      </div>`]
+
+//      for(var i=0; i< gettingTodosFromLS.length; i++ ){
+//         finalstructure += `<div id= "content">
+//         <p>${gettingTodosFromLS[i]}</p>
+//         <i onclick = "deleteTodo(${i})" class="fa-solid fa-trash"></i>
+//         </div>`
+//  }
+
+//   divFromhtml.innerHTML = finalstructure;
+// }
+// cs()
+
+
+// function cs(){
+//     var mainDiv = document.getElementById("displayTodos");
+//     var div = document.createElement("div");
+//     var h1 = document.createElement("h1");
+//     h1.innerText = "Hello World";
+//     div.append(h1);
+//     mainDiv.append(div);
+// }
+// cs()
 
 
 
